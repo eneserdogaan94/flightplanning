fb6f9a7f20d46dd6b7e54a8a9b60c98038d6ab79
Enes Erdoğan <eneserdogaan94@gmail.com>
2025-01-28 00:10:05 +0300

    Initial commit: flight planning initial commit

---
+++ src/test/java/com/example/flightplanning/FlightPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
